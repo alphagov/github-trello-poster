215a9e800702ae8047500bffa4f901b193270514
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2024-06-19 13:10:47 +0100

    Refactor TrelloPoster and rewrite its specs

--- lib/trello_poster.rb
+++ lib/trello_poster.rb
@@ -1,8 +1,8 @@
 require "trello"
 
 class TrelloPoster
-  def initialize
-    @client = Trello::Client.new(
+  def initialize(client = nil)
+    @client = client || Trello::Client.new(
       developer_public_key: ENV["TRELLO_PUBLIC_KEY"],
       member_token: ENV["TRELLO_MEMBER_TOKEN"],
     )
@@ -11,12 +11,9 @@ class TrelloPoster
   def post!(pr_url, trello_card_id, pr_closed)
     trello_card = client.find(:card, trello_card_id)
     pr_checklist = find_or_create_pr_checklist(trello_card)
+    item = find_or_create_checklist_item(pr_checklist, pr_url)
 
-    if find_pr_on_checklist(pr_checklist, pr_url).nil?
-      pr_checklist.add_item(pr_url, false, "bottom")
-    end
-
-    mark_as_complete(trello_card, pr_url) if pr_closed
+    mark_as_complete(pr_checklist, item) if pr_closed
   end
 
 private
@@ -24,22 +21,17 @@ private
   attr_reader :client
 
   def find_or_create_pr_checklist(trello_card)
-    trello_card
-      .checklists
-      .find { |c| ["pull requests", "prs"].include? c.name.downcase }
-      .then { |c| c || client.create(:checklist, "name" => "Pull Requests", "idCard" => trello_card.id) }
+    trello_card.checklists.find { |c| ["pull requests", "prs"].include? c.name.downcase } ||
+      client.create(:checklist, "name" => "Pull Requests", "idCard" => trello_card.id)
   end
 
-  def mark_as_complete(trello_card, pr_url)
-    checklist = find_or_create_pr_checklist(trello_card)
-    item = find_pr_on_checklist(checklist, pr_url)
-    unless item.nil?
-      checklist.update_item_state(item["id"], "complete")
-      checklist.save
-    end
+  def find_or_create_checklist_item(checklist, pr_url)
+    checklist.check_items.find { |item| item["name"].include? pr_url } ||
+      checklist.add_item(pr_url, false, "bottom")
   end
 
-  def find_pr_on_checklist(checklist, pr_url)
-    checklist.check_items.find { |item| item["name"].include? pr_url }
+  def mark_as_complete(checklist, item)
+    checklist.update_item_state(item["id"], "complete")
+    checklist.save
   end
 end
