alphagov/github-trello-poster

commit 1bce5a85977fc0092bed966d98a997a81aa930b5

Drive-by refactor of TrelloPoster

lib/trello_poster.rb
@@ -9,53 +9,37 @@ class TrelloPoster
   end
 
   def post!(pr_url, trello_card_id, pr_closed)
-    trello_card = access_trello_card(trello_card_id)
-    pr_checklist = check_for_pr_checklist(trello_card)
-    post_github_pr_url(pr_url, pr_checklist)
-    check_off_pull_request(trello_card, pr_url) if pr_closed
+    trello_card = client.find(:card, trello_card_id)
+    pr_checklist = find_or_create_pr_checklist(trello_card)
+
+    if find_pr_on_checklist(pr_checklist, pr_url).nil?
+      pr_checklist.add_item(pr_url, false, "bottom")
+    end
+
+    mark_as_complete(trello_card, pr_url) if pr_closed
   end
 
 private
 
   attr_reader :client
 
-  def access_trello_card(trello_card_id)
-    client.find(:card, trello_card_id)
+  def find_or_create_pr_checklist(trello_card)
+    trello_card
+      .checklists
+      .find { |c| ["pull requests", "prs"].include? c }
+      .then { |c| c || client.create(:checklist, "name" => "Pull Requests", "idCard" => trello_card.id) }
   end
 
-  def check_for_pr_checklist(trello_card)
-    checklist = trello_card.checklists.detect do |checklist|
-      return checklist if is_a_pr_checklist?(checklist)
-    end
-    create_pr_checklist(trello_card) if checklist.nil?
-  end
-
-  def post_github_pr_url(pr_url, pr_checklist)
-    item = pr_url_on_checklist(pr_checklist, pr_url)
-    pr_checklist.add_item(pr_url, checked = false, position = "bottom") if item.nil?
-  end
-
-  def check_off_pull_request(trello_card, pr_url)
-    checklist = check_for_pr_checklist(trello_card)
-    item = pr_url_on_checklist(checklist, pr_url)
+  def mark_as_complete(trello_card, pr_url)
+    checklist = find_or_create_pr_checklist(trello_card)
+    item = find_pr_on_checklist(checklist, pr_url)
     unless item.nil?
       checklist.update_item_state(item["id"], "complete")
       checklist.save
     end
   end
 
-  def create_pr_checklist(trello_card)
-    client.create(:checklist,
-                  "name" => "Pull Requests",
-                  "idCard" => trello_card.id)
-  end
-
-  def is_a_pr_checklist?(checklist)
-    checklist.name.downcase == "pull requests" ||
-      checklist.name.downcase == "prs"
-  end
-
-  def pr_url_on_checklist(checklist, pr_url)
-    checklist.check_items.find { |item| item["name"] =~ /#{Regexp.quote(pr_url)}/i }
+  def find_pr_on_checklist(checklist, pr_url)
+    checklist.check_items.find { |item| item["name"].include? pr_url }
   end
 end

commit 8cf9700852d637f573ec9a1360d338670274030d

Fix bug causing duplicate PR checklists
This bug was introduced in 1bce5a8, but was not caught by the existing
test suite

lib/trello_poster.rb
@@ -26,7 +26,7 @@ private
   def find_or_create_pr_checklist(trello_card)
     trello_card
       .checklists
-      .find { |c| ["pull requests", "prs"].include? c }
+      .find { |c| ["pull requests", "prs"].include? c.name.downcase }
       .then { |c| c || client.create(:checklist, "name" => "Pull Requests", "idCard" => trello_card.id) }
   end
 